4d5fb9fe7dd01d8e37c163d793d6466acab52b6f
allanzhuo <23962957@qq.com>
2019-05-06 14:54:35 +0800

    完成左侧菜单开发

--- yyms-web/src/main/resources/static/js/admin/admin_index.js
+++ yyms-web/src/main/resources/static/js/admin/admin_index.js
@@ -3,6 +3,66 @@ layui.use(['element', 'layer'], function () {
         element = layui.element,
         layer = layui.layer;
     element.render();
+
+    init();
+    function init() {
+        var windowWidth = window.innerWidth;
+        var leftMenus = '';
+        $.post("/admin/menu/init", function (res) {
+            if (res.code == 200) {
+                for (let index in res.data) {
+                    if (res.data[index].children === null || res.data[index].children.length == 0) {
+                        leftMenus += '<li class="layui-nav-item">' +
+                            '<a href="' + res.data[index].url + '" lay-tips="' + res.data[index].title + '">' +
+                            '<i class="' + res.data[index].icon + '"></i>' +
+                            '<cite>' + res.data[index].title + '</cite>' +
+                            '</a>' +
+                            '</li>';
+                    } else {
+                        leftMenus += '<li class="layui-nav-item">' +
+                            '<a href="javascript:;" lay-tips="' + res.data[index].title + '">' +
+                            '<i class="' + res.data[index].icon + '"></i>' +
+                            '<cite>' + res.data[index].title + '</cite>' +
+
+                            '</a>' +
+                            '<dl class="layui-nav-child">';
+                        for (let childrenIndex in res.data[index].children) {
+                            leftMenus += '<dd>' +
+                                '<a href="' + res.data[index].children[childrenIndex].url + '">' +
+                                res.data[index].children[childrenIndex].title
+                                /*    '<i class="'+res.data[index].children[childrenIndex].icon+'"></i>'+
+                                    '<cite>'+res.data[index].children[childrenIndex].title+'</cite>'+*/
+                                + '</a>' +
+                                '</dd>';
+                        }
+                        leftMenus += '</dl></li>';
+                    }
+                };
+                var leftMenu = document.getElementById('left-menu-temp');
+                leftMenu.innerHTML = leftMenus;
+                element.render('nav', 'left-menu-temp');
+                if (windowWidth < 993) {
+                    $("#flexible-icon").removeClass("layui-icon-spread-right").addClass("layui-icon-spread-left");
+                } else {
+                    $('*[lay-tips]').on('mouseenter', function () {
+                        var select = $("#flexible").attr("lay-select");
+                        if (select != undefined) {
+                            var content = $(this).attr('lay-tips');
+                            this.index = layer.tips('<div >' + content + '</div>', this, {
+                                time: -1,
+                                maxWidth: 280,
+                                tips: [2, '#000']
+                            });
+                        }
+                    }).on('mouseleave', function () {
+                        layer.close(this.index);
+                    });
+                }
+            };
+        });
+    };
+
+    /**监听侧边伸缩按钮*/
     element.on('nav(nav-icon)', function (elem) {
         var windowWidth = window.innerWidth;
         var event = $(elem).attr("layadmin-event");
@@ -13,16 +73,21 @@ layui.use(['element', 'layer'], function () {
                 if (windowWidth >= 993) {
                     $("body").removeClass("layadmin-side-spread-sm").addClass("layadmin-side-shrink");
                     $("#left-menu-temp").addClass("side-shrink");
+                    $(elem).find(".layui-icon").removeClass("layui-icon-spread-right").addClass("layui-icon-spread-left");
                 } else {
                     $("body").removeClass("layadmin-side-shrink").addClass("layadmin-side-spread-sm");
                     $("#left-menu-temp").removeClass("side-shrink");
+                    $(elem).find(".layui-icon").removeClass("layui-icon-spread-left").addClass("layui-icon-spread-right");
                 }
-                $(elem).find(".layui-icon").removeClass("layui-icon-spread-right").addClass("layui-icon-spread-left");
             } else {
                 $(elem).removeAttr("lay-select");
                 $("body").removeClass("layadmin-side-shrink").removeClass("layadmin-side-spread-sm");
                 $("#left-menu-temp").removeClass("side-shrink");
-                $(elem).find(".layui-icon").removeClass("layui-icon-spread-left").addClass("layui-icon-spread-right");
+                if (windowWidth >= 993) {
+                    $(elem).find(".layui-icon").removeClass("layui-icon-spread-left").addClass("layui-icon-spread-right");
+                } else {
+                    $(elem).find(".layui-icon").removeClass("layui-icon-spread-right").addClass("layui-icon-spread-left");
+                }
             }
         }
         if (event === 'refresh') {
@@ -33,6 +98,7 @@ layui.use(['element', 'layer'], function () {
 //            }
     });
 
+    /**监听侧边菜单，点击显示全部*/
     element.on('nav(left-menu-temp)', function (elem) {
         $("#flexible").removeAttr("lay-select");
         $("body").removeClass("layadmin-side-shrink").removeClass("layadmin-side-spread-sm");
@@ -40,62 +106,19 @@ layui.use(['element', 'layer'], function () {
         $("#flexible-icon").removeClass("layui-icon-spread-left").addClass("layui-icon-spread-right");
     });
 
-    $('*[lay-tips]').on('mouseenter', function () {
-        var select = $("#flexible").attr("lay-select");
-        if (select != undefined) {
-            var content = $(this).attr('lay-tips');
-            this.index = layer.tips('<div >' + content + '</div>', this, {
-                time: -1,
-                maxWidth: 280,
-                tips: [2, '#000']
-            });
+    $(window).resize(function () {
+        $("body").removeClass("layadmin-side-spread-sm").removeClass("layadmin-side-shrink");
+        var windowWidth = window.innerWidth;
+        if (windowWidth < 993) {
+            $("#flexible-icon").removeClass("layui-icon-spread-right").addClass("layui-icon-spread-left");
+        } else {
+            $("#flexible-icon").removeClass("layui-icon-spread-left").addClass("layui-icon-spread-right");
         }
-    }).on('mouseleave', function () {
-        layer.close(this.index);
     });
 
-    //init();
-    function init(){
-        var leftMenus = '';
-        $.post("/admin/menu/init", function (res) {
-            if (res.code == 200) {
-                for(let index in res.data) {
-                    console.log(res.data[index].children.length)
-                    console.log(res.data[index].children)
-                    if(res.data[index].children===null || res.data[index].children.length==0){
-                        leftMenus += '<li class="layui-nav-item">'+
-                            '<a href="'+res.data[index].url+'" lay-tips="'+res.data[index].title+'">'+
-                            '<i class="'+res.data[index].icon+'"></i>'+
-                            '<cite>'+res.data[index].title+'</cite>'+
-                            '</a>'+
-                            '</li>';
-                        console.log(res.data[index].children.length+"000")
-                    } else {
-                        leftMenus += '<li class="layui-nav-item">'+
-                            '<a href="javascript:;" lay-tips="'+res.data[index].title+'">'+
-                            '<i class="'+res.data[index].icon+'"></i>'+
-                            '<cite>'+res.data[index].title+'</cite>'+
-
-                            '</a>'+
-                            '<dl class="layui-nav-child">';
-                            for(let childrenIndex in res.data[index].children){
-                                leftMenus += '<dd>'+
-                                '<a href="'+res.data[index].children[childrenIndex].url+'">'+
-                                    res.data[index].children[childrenIndex].title
-                            /*    '<i class="'+res.data[index].children[childrenIndex].icon+'"></i>'+
-                                '<cite>'+res.data[index].children[childrenIndex].title+'</cite>'+*/
-                                    +'</a>'+
-                                '</dd>';
-                            }
-                        leftMenus += '</dl></li>';
-                    }
-                };
-                console.log(leftMenus)
-             var leftMenu = document.getElementById('left-menu-temp');
-                leftMenu.innerHTML = leftMenus;
-            }
-        });
-    }
+    $(".layadmin-body-shade").click(function () {
+        $(".layadmin-flexible>a").trigger("click");
+    });
 
     $.ajaxSetup({
         complete: function (XMLHttpRequest, textStatus) {
