8f6e30b918046c666cb265f58f6a2c037604ab0f
allanzhuo <23962957@qq.com>
2019-05-09 17:55:14 +0800

    新增通知公告表

---
+++ yyms-web/src/main/java/net/laoyeye/yyms/pojo/domain/NoticeDO.java
@@ -0,0 +1,68 @@
+package net.laoyeye.yyms.pojo.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import static java.lang.Boolean.TRUE;
+import static java.time.LocalDateTime.now;
+
+
+/**
+ * @description: 通知公告表
+ * @author: laoyeye.net
+ * @date: 2019/5/9 19:10
+ */
+@Data
+@Builder(toBuilder=true)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "sys_notice")
+@org.hibernate.annotations.Table(appliesTo = "sys_notice",comment="通知公告表")
+public class NoticeDO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(generator = "id")
+	@GenericGenerator(name = "id", strategy = "net.laoyeye.yyms.pojo.strategy.IdentifierGeneratorImpl")
+	@Column(updatable = false, nullable = false, columnDefinition="bigint COMMENT '主键'")
+	private Long id;
+
+	@Column(nullable = false, columnDefinition="varchar(20) COMMENT '标题'")
+	private String noticeTitle;
+
+	@Column(nullable = false, columnDefinition="text COMMENT '内容'")
+	private String noticeContent;
+
+	@Column(nullable = false, columnDefinition = "tinyint(1) COMMENT '是否有效'")
+	@Builder.Default
+	private Boolean noticeStatus = TRUE;
+
+	@Column(updatable = false, nullable = false, length = 20)
+	private String createUser;
+
+	@CreationTimestamp
+	@Builder.Default
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createTime = now();
+
+	@Column(nullable = false, length = 20)
+	private String updateUser;
+
+	@UpdateTimestamp
+	@Builder.Default
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Column(nullable = false)
+	private LocalDateTime updateTime = now();
+
+}
---
+++ yyms-web/src/main/java/net/laoyeye/yyms/pojo/domain/NoticeRecordDO.java
@@ -0,0 +1,54 @@
+package net.laoyeye.yyms.pojo.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Date;
+
+import static java.lang.Boolean.FALSE;
+
+/**
+ * @description: 通知通告发送记录
+ * @author: laoyeye.net
+ * @date: 2019/5/9 19:37
+ */
+@Data
+@Builder(toBuilder=true)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "sys_notice_record")
+@org.hibernate.annotations.Table(appliesTo = "sys_notice_record",comment="通知公告发送记录")
+public class NoticeRecordDO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(generator = "id")
+    @GenericGenerator(name = "id", strategy = "net.laoyeye.yyms.pojo.strategy.IdentifierGeneratorImpl")
+    @Column(updatable = false, nullable = false, columnDefinition="bigint COMMENT '主键'")
+    private Long id;
+
+    @NotBlank(message = "通知ID不能为空")
+    @Column(nullable = false, columnDefinition="bigint(20) COMMENT '通知ID'")
+    private Long noticeId;
+
+    @NotBlank(message = "用户ID不能为空")
+    @Column(nullable = false, columnDefinition="bigint(20) COMMENT '用户ID'")
+    private Long userId;
+
+    @Column(nullable = false, columnDefinition = "tinyint(1) COMMENT '是否已读'")
+    @Builder.Default
+    private Boolean readFlg = FALSE;
+
+    @UpdateTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column
+    private Date readDate;
+}
