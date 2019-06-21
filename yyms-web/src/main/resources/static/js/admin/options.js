106ee7b881b753aa87b995ec772e6384a976470c
allanzhuo <23962957@qq.com>
2019-06-21 17:45:56 +0800

    配置页修改

--- yyms-web/src/main/resources/static/js/admin/options.js
+++ yyms-web/src/main/resources/static/js/admin/options.js
@@ -6,7 +6,43 @@ layui.use('form', function () {
     form.on('submit(website-set)', function (data) {
         $.post("/admin/options/save", data.field, function (res) {
             if (res.code == 200) {
+                layer.msg(res.msg, {icon: 1});
+            } else {
+                layer.msg(res.msg, {icon: 2});
+            }
+        });
+        return false;
+    });
 
+    //监听提交
+    form.on('submit(smtp-set)', function (data) {
+        $.post("/admin/options/save", data.field, function (res) {
+            if (res.code == 200) {
+                layer.msg(res.msg, {icon: 1});
+            } else {
+                layer.msg(res.msg, {icon: 2});
+            }
+        });
+        return false;
+    });
+
+    //监听提交
+    form.on('submit(storage-set)', function (data) {
+        $.post("/admin/options/save", data.field, function (res) {
+            if (res.code == 200) {
+                layer.msg(res.msg, {icon: 1});
+            } else {
+                layer.msg(res.msg, {icon: 2});
+            }
+        });
+        return false;
+    });
+
+    //监听提交
+    form.on('submit(global-set)', function (data) {
+        $.post("/admin/options/save", data.field, function (res) {
+            if (res.code == 200) {
+                layer.msg(res.msg, {icon: 1});
             } else {
                 layer.msg(res.msg, {icon: 2});
             }
