d2cfab8d7bfab499860f57ac4f3177ecc940fb9e
allanzhuo <23962957@qq.com>
2019-06-29 14:04:47 +0800

    角色管理

--- yyms-web/src/main/resources/static/js/admin/role.js
+++ yyms-web/src/main/resources/static/js/admin/role.js
@@ -2,14 +2,8 @@ layui.use(['form', 'table', 'layer', 'laydate'], function () {
     var $ = layui.$
         , table = layui.table
         , form = layui.form
-        , layer = layui.layer
-        , laydate = layui.laydate;
-
-    laydate.render({
-        elem: '#queryDate'
-        , type: 'datetime'
-        , range: true
-    });
+        , layer = layui.layer;
+    
     table.render({
         elem: '#role-table'
         , url: "/admin/role/list"
@@ -19,14 +13,14 @@ layui.use(['form', 'table', 'layer', 'laydate'], function () {
             {type: 'checkbox'}
             , {type: 'numbers'}
             , {field: 'roleName', width: 150, title: '角色名称', align: 'center'}
-            , {field: 'roleCode', width: '40%', title: '角色编码', align: 'center'}
+            , {field: 'roleCode', width: 150, title: '角色编码', align: 'center'}
             , {field: 'remark', width: '40%', title: '备注', align: 'center'}
             , {title: '状态', width: 100, align: 'center', toolbar: '#statusBar'}
             , {field: 'createUser', width: 150, title: '创建人', align: 'center'}
             , {field: 'createTime', width: 200, title: '创建时间', align: 'center'}
             , {field: 'updateUser', width: 150, title: '修改人', align: 'center'}
             , {field: 'updateTime', width: 200, title: '修改时间', align: 'center'}
-            , {title: '操作', width: 150, align: 'center', fixed: 'right', toolbar: '#noticeBar'}
+            , {title: '操作', width: 150, align: 'center', fixed: 'right', toolbar: '#roleBar'}
         ]]
         , page: true
     });
@@ -34,23 +28,21 @@ layui.use(['form', 'table', 'layer', 'laydate'], function () {
     //监听提交
     form.on('submit(btn-search)', function (data) {
         //执行重载
-        table.reload('notice-table', {
+        table.reload('role-table', {
             page: {
                 curr: 1 //重新从第 1 页开始
             }
             , where: {
-                startDate: data.field.queryDate.substring(0, 19),
-                endDate: data.field.queryDate.substring(22, 41),
-                noticeTitle: data.field.noticeTitle
+                roleName: data.field.roleName
             }
         });
         return false;
     });
 
-    form.on('switch(noticeStatus)', function (obj) {
-        $.post("/admin/notice/edit/status", {id: this.value, noticeStatus: obj.elem.checked}, function (res) {
+    form.on('switch(roleStatus)', function (obj) {
+        $.post("/admin/role/edit/status", {id: this.value, roleStatus: obj.elem.checked}, function (res) {
             if (res.code == 200) {
-                layer.msg("修改状态成功", {icon:1});
+                layer.msg(res.msg, {icon:1});
             } else {
                 layer.msg("修改失败," + res.msg, {icon:2});
             }
@@ -124,41 +116,45 @@ layui.use(['form', 'table', 'layer', 'laydate'], function () {
         }
     });
 
-    $("#remove-batch").on("click", function () {
-        var checkStatus = table.checkStatus('notice-table');
-        var data = checkStatus.data;
-        if (data.length == 0) {
-            layer.msg('请先选择要删除的数据', {time: 3000, icon:0});
-            return;
-        }
-        layer.confirm("确认要删除选中的【" + data.length + "】条数据吗?", function (index) {
-            var ids = new Array();
-            // 遍历所有选择的行数据，取每条数据对应的ID
-            for (var i = 0; i < data.length; i++) {
-                ids[i] = data[i].id;
-            }
-            $.post("/admin/notice/removeBatch", {ids: ids}, function (res) {
-                if (res.code == 200) {
-                    layer.msg(res.msg, {icon:1});
-                    //执行重载
-                    var queryDate = $("#queryDate").val();
-                    var noticeTitle = $("#noticeTitle").val();
-                    table.reload('notice-table', {
-                        page: {
-                            curr: 1 //重新从第 1 页开始
-                        }
-                        , where: {
-                            startDate: queryDate.substring(0, 19),
-                            endDate: queryDate.substring(22, 41),
-                            noticeTitle: noticeTitle
-                        }
-                    });
-                } else {
-                    layer.msg(res.msg, {icon:2});
+    var active = {
+        add: function () {
+            layer.open({
+                type: 1,
+                title: '添加菜单',
+                shadeClose: true,
+                maxmin: true, //开启最大化最小化按钮
+                area: ['500px', '480px'], //宽高
+                content: $("#notice-edit-tpl").html()
+                ,success:function(layero, index) {
+                    form.render('select');
                 }
             });
-            layer.close(index);
-        });
+        },
+        del: function () {
+            var ids = treeTable.checked(table)
+            if (ids.length == 0) {
+                layer.msg('请先选择要删除的数据', {time: 3000, icon:0});
+                return;
+            }
+            layer.confirm("确认要删除选中的【" + ids.length + "】条数据吗?", function (index) {
+                $.post("/admin/menu/remove", {ids: ids}, function (res) {
+                    if (res.code === 200) {
+                        layer.msg(res.msg, {icon:1});
+                        refresh();
+                    } else {
+                        layer.msg(res.msg, {icon:2});
+                    }
+                });
+                layer.close(index);
+            });
+        }
+    };
+
+    $('#table-tools').find('.layui-btn').off('click').on('click', function () {
+        var type = $(this).data('type');
+        active[type] ? active[type].call(this) : '';
+        return false;
     });
+
 
 });
