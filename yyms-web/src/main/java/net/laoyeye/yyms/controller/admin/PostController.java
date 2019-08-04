f3660d63f80d578c66dc10a5bf5bdc66736d710a
allanzhuo <23962957@qq.com>
2019-08-04 17:47:59 +0800

    新增编辑

---
+++ yyms-web/src/main/java/net/laoyeye/yyms/controller/admin/PostController.java
@@ -0,0 +1,21 @@
+package net.laoyeye.yyms.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @description: 随笔页面
+ * @author: zhangzhuo
+ * @date: 2019/8/4 17:35
+ */
+@Controller
+@RequestMapping("/admin/post")
+public class PostController {
+    @GetMapping("/edit")
+    public String index(Model model) {
+
+        return "admin/post_edit_md";
+    }
+}
